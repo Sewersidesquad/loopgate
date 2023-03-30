84da698447d9b06858641f5d98ebee1783f92fd1
LiamDoesWeb <you@example.com>
2023-03-30 17:38:02 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,23 +21,209 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeibj6ze2nm4uhcskqhdyzcgzzmgsyulpftpzdmd5ygypktcp345yku", // vipskin
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' to unlock this.
+      "0x9626967022705f80d2215333a1ab931f01e3089c9c2d74f6a2219b2f32369393",
+    ], 
   },
   {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
+    cid: "bafybeihdbbvpzf3uycfp5ovx3bqi6ufblf35qckbielh4amsvgiti2jjoq", // ravensclaw
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
+      "0x5a741aeec046e161be605397c7cc8c479c6faade324b4404e17f1fe260d2951b",
+    ], 
   },
   {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
+    cid: "bafybeibc3os5tkbyq4uokmc6lmigy4kt4fh44vx6is3mch34fqs5fdecam", // starchaser
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+      "0x3f0db3a5f4a76211866099e3a6ebae0dd767a0d1a38827dc97029eab0a0ef0d1",
+    ], 
+  },
+  {
+    cid: "bafybeib53otnyvtxrg64t257kvwklf3moj5ydje7vjx6ybfx5nbx37nyuy", //snappy 5
+    nftId: [
+      "0xc639d2875d11ef565142b38e87637b546a6f525cb5a4a797ca145374151dacf4",
+    ], 
+  },
+  {
+    cid: "bafybeia5rz2owmz2o4wgl7723nwb37zhqhpjnps7r5rbfqmffr447a37qm", // snappy 4
+    nftId: [
+      "0x09f8fb1db609ff562af0678fb9e19e12a07a2eb70a98b15f3c8c118fd836af2b",
+    ], 
+  },
+  {
+    cid: "bafybeico2ffbsii7cvr4zpwdl3pitylmrgjny4fvzg54xzgqo7wap4tsti", // snappy 3
+    nftId: [
+      "0xb829c661289c4ee8d577bf1c026a6bf1058065f9b55c6116aae039678ac1c8d1",
+    ], 
+  },
+  {
+    cid: "bafybeieqai4g2pplsr7xmgwubfvdfpjxyfukik4hm2arakuheiwasdbyrm", // snappy 2
+    nftId: [
+      "0x2e9a77c44ae36e4c1aa1418697a817ebd937dbea548bd5e7ae6d40df27401eec",
+    ], 
+  },
+  {
+    cid: "bafybeiamyf24hpiba3sve63o6xyqoo33ajwv42pi3wtyxk4whgunkctexu", // snappy 1
+    nftId: [
+      "0x9c9290ff78ff0dec01cb806f5e310850b2d351720a317aacc44b9940183fa87e",
+    ], 
+  },
+  {
+    cid: "bafybeibknixz55tunyzze2p7n25cg3yitzhhhculbodlxfhp3coh3enp5i", // robort
+    nftId: [
+      "0x1cdabc4387555b3f7eae57d9f61561903bb50d6863132d7a748e723d99888889",
+    ], 
+  },
+  {
+    cid: "bafybeiag2hpamrqsszv7oyywlqx6xngqenhuw6cszyf7ukrdfqpk5hipza", // pistonHead
+    nftId: [
+      "0xa15b3db05d3fef3c99f6f0bc8755a8e940ef2fe450e4c2afe5314bbe391d9587",
+    ], 
+  },
+  {
+    cid: "bafybeibefzm5jftvhjsgxeqz4vnj75h36bmydlk7z2s4dqnt7s43epwiia", // insect
+    nftId: [
+      "0x4514f8a9c240b3dd3298e124b891cca7d2f3339bde496e22e2d6f3f12f9b14f8",
+    ], 
+  },
+  {
+    cid: "bafybeibisy2apupubg4vk7ulvcgix2d352xoljmszb62qx33wrcdgggjpu", // HT
+    nftId: [
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeidj35oee6wrkjwpcfvkqv5dipcdk4zsebvjvq3aqkkwnjk46egcba", // HornDawg
+    nftId: [
+      "0xe76b9eb26f532cd4c4bdf6cd84869926e1c79eae255c350dd0db03fc5d2c15e4",
+    ], 
+  },
+  {
+    cid: "bafybeibaxamqfg2tlhd3pmab7vlskjosiayvesoidli2o6sqkt7hcmkshy", // fugly
+    nftId: [
+      "0x6fb87b3a412d04f1d8a2b08c67cb350d2dafeed7aefda508325b5ef80adec27e",
+    ], 
+  },
+  {
+    cid: "bafybeign33gvhvxkww4wc5lxuyq4z4pylnommmfcivhyy6mlofkxyipfti", // shotgun
+    nftId: [
+      "0xf1392ba42b4ad63558a1d1cd73ed4520468914cd01b5228d489a23ef5760a8a2",
+    ], 
+  },
+  {
+    cid: "bafybeihfkg57vsa2hx3hpfgn2rop2iv5j7oy2icl7q3rra3bhvlmmi7ktq", // pistol
+    nftId: [
+      "0xd16a301c6fb37baca4869985e6f436a8435e436b8def6bbbc415c21992787e08",
+    ], 
+  },
+  {
+    cid: "bafybeigl52gmehpajthrouytnmkdfmam3j4mnisyy7a6dobhhh5fhusihu", // red dawn
+    nftId: [
+      "0x20e5b95eceacdb3180d2e651654aadd696507cd210277509f7ddf4e8997a7d31",
+    ], 
+  },
+  {
+    cid: "bafybeia3jdjjcgn26ldcgk7qxxbsxiw5sibvr4xvhotagtff73hqo4orki", // emerald
+    nftId: [
+      "0x15ff1f52e50ce198a23874d5ecf0888b9c5369e2d4fd399e82b9cd8c6084cb5a",
+    ], 
+  },
+  {
+    cid: "bafybeiarwcspg4pnads6ppw4s2rbpilrwnka7eou2eflz7jpme74p7jn5e", // blue neb
+    nftId: [
+      "0xcf2531cc107b23c5bb4f5d5656410c7d85dafe3334d147d15624a6c5c2a25123",
+    ], 
+  },
+  {
+    cid: "bafybeifjnonjhnog27b5ewhi7n3qndmyxdceq572zrynnuv5dxanlzrbrm", // creep
+    nftId: [
+      "0xee85d578382e49c423475910505b39ec320039882c5e65a2b5c810dcdc675f6c",
+    ], 
+  },
+  {
+    cid: "bafybeicrt7xubtfbzskxxiljmg4czqtwmxiot33cx4t5iwzbqjsnn57tay", // crab
+    nftId: [
+      "0x399363e2da38aaaf6dc44d455e8d9ca240cbb35dc0d3a6a8fcba4d5fa0b09b31",
+    ], 
+  },
+  {
+    cid: "bafybeihnood7szldxhe57f5df3k5gphejcmlec4diqpjabvrdnp2elyrze", // topaz
+    nftId: [
+      "0xa7be808490f219a03d0b7ba660d1c68823133f04f22415fa76d5fd52690a6d79",
+    ], 
+  },
+  {
+    cid: "bafybeiccdlsc7ohvoqedo6et7cixn6riojyt7yqbsimjetvom2up7sxlzm", // sillimanite
+    nftId: [
+      "0x3d323b801440df902740725d1bfa14ef9fb838634e3acef018a14eaf83578a86",
+    ], 
+  },
+  {
+    cid: "bafybeidbsr5o3alsrwmc5v6ybenoa6pk26gkg2jpvrq2bn5d2ghulfakku", // Blruby
+    nftId: [
+      "0x23e5c30f3e45d8b05d604a532b8fbfe42e42fac5ac788336758652c793794d83",
+    ], 
+  },
+  {
+    cid: "bafybeibdlviny3tbv3yteke4ewilxw3bkdq7omed6eomqh36dbhoyg473q", // Peridot
+    nftId: [
+      "0x0da1f4687ce784b19c54fabc83afedf541598f4997fada868c1cc2824cf7e432",
+    ], 
+  },
+  {
+    cid: "bafybeigqnk334dhzravhvvwaapqt77vlvmvuvifify5iknxhqtimaokx2m", // Kunzite
+    nftId: [
+      "0x0a6256af0b1c14cb2654ab4c7dee2daa54b9dc5d032ffdc185719e608623c35c",
+    ], 
+  },
+  {
+    cid: "bafybeih5dcvef53r3d6oiosijd2fpcbkzwv5powuo35vmylsrtahvlimxu", // RavensClawHT
+    nftId: [
+      "0x5a741aeec046e161be605397c7cc8c479c6faade324b4404e17f1fe260d2951b",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeia46e6g4ypp77bgerurodbikyk53uuvcq7c5gaidhz7p7gxxneuwu", // StarchaserHT
+    nftId: [
+      "0x3f0db3a5f4a76211866099e3a6ebae0dd767a0d1a38827dc97029eab0a0ef0d1",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeie6caydgcd5mc5o2haj37n7wc6kv7qjtav5zavajkkfvwbrstygzq", // TopazHT
+    nftId: [
+      "0xa7be808490f219a03d0b7ba660d1c68823133f04f22415fa76d5fd52690a6d79",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeicxrzfxgmi7uwzarpx3qrjoomb3pre5filyldkfi7fw6okyuv77yi", // SillimaniteHT
+    nftId: [
+      "0x3d323b801440df902740725d1bfa14ef9fb838634e3acef018a14eaf83578a86",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeichbodfbent57x6p2w2zdo54jrr7sf4qecjfa3kflv22ccxhzuld4", // RubyHT
+    nftId: [
+      "0x23e5c30f3e45d8b05d604a532b8fbfe42e42fac5ac788336758652c793794d83",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeifniqxawletc4cgmpkiqzrl2hev3v7smu3igxudwml5gwgdylvtka", // PeridotHT
+    nftId: [
+      "0x0da1f4687ce784b19c54fabc83afedf541598f4997fada868c1cc2824cf7e432",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
+  },
+  {
+    cid: "bafybeieje44zxfuahhbgt6xz2n54ohmw3jf6qvlwlfeor2zgwswpkviy24", // KunziteHT
+    nftId: [
+      "0x0a6256af0b1c14cb2654ab4c7dee2daa54b9dc5d032ffdc185719e608623c35c",
+      "0xd597eaf0cd202aae4fab5b82cab82522f2e9c0353c65e79c3d80b4ef97219a49",
+    ], 
   },
 ];
 
